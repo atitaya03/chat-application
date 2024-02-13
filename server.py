680d63703fd8e22998d89849328c761e9c075898
Atitaya <priow111555@gmail.com>
2024-02-14 00:56:48 +0700

    chat application

---
+++ server.py
@@ -0,0 +1,52 @@
+import threading
+import socket
+
+host = '127.0.0.1'
+port = 2222
+
+sever = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sever.bind((host,port))
+sever.listen()
+
+clients = []
+nicknames = []
+
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} left the chat!'.encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        client, address = sever.accept()
+        print(f"Connected with {str(address)}")
+
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'{nickname} joined the chat')
+        broadcast(f'{nickname} joined the chat!'.encode('ascii'))
+        client.send(f'Connected to the sever!'.encode('ascii'))
+
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("Sever is listening...")
+receive()
